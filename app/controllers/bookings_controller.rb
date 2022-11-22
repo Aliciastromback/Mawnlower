fe3e1070bf818764aedbbbee655df0abcffc8e61
Joshua Smith <jmscodingde@gmail.com>
2022-11-22 14:54:10 +0100

    index method and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show; end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :lawnmower_id)
+  end
 end

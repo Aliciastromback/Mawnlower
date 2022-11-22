41f10490aa0bba5e8ce5bd85395a6b18750fa222
Joshua Smith <jmscodingde@gmail.com>
2022-11-22 15:41:04 +0100

    booking controller methods, views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,28 +1,48 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :update, :destroy]
+  before_action :set_lawnmower, only: [:new, :create]
 
   def index
     @bookings = Booking.all
   end
 
   def show; end
-  
+
   def edit; end
+
+  def new
+    @booking = booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.lawnmower = @lawnmower
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def update
     @booking.update(booking_params)
     redirect_to booking_path(@booking)
   end
-  
+
   def destroy
     @booking.destroy
     redirect_to bookings_path, status: :see_other
   end
-  
+
   private
 
   def set_booking
     @booking = Booking.find(params[:id])
+  end
+
+  def set_lawnmower
+    @lawnmower = Lawnmower.find(params[:id])
   end
 
   def booking_params

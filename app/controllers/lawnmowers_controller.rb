c70fdeace7cc6f0ab654720d75f2225ebc3b8059
Alicia Stromback <aliciastromback@gmail.com>
2022-11-22 11:47:31 +0100

    edit_update_actions

--- app/controllers/lawnmowers_controller.rb
+++ app/controllers/lawnmowers_controller.rb
@@ -1,2 +1,17 @@
 class LawnmowersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    @lawnmower = Lawnmower.find(params[:id])
+    @lawnmower.update(lawnmower_params)
+    redirect_to lawmower_path(@lawnmower)
+  end
+
+  def destroy
+    @lawnmower = Lawnmower.find(params[:id])
+    @lawnmower.destroy
+    redirect_to lawnmowers_path, status: :see_other
+  end
 end

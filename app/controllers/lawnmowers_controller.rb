7b895d3d3824491d47c6e0991436455be49b8a46
Joshua Smith <jmscodingde@gmail.com>
2022-11-22 11:55:01 +0100

    new crete

--- app/controllers/lawnmowers_controller.rb
+++ app/controllers/lawnmowers_controller.rb
@@ -1,2 +1,21 @@
 class LawnmowersController < ApplicationController
+
+  def new
+    @lawnmower = Lawnmower.new
+  end
+
+  def create
+    @lawnmower = Lawnmower.new(lawnmower_params)
+    if @lawnmower.save
+      redirect_to lawnmower_path(@lawnmower)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def lawnmower_params
+    params.require(:lawnmower).permit(:title, :price, :description, :speed, :user_id)
+  end
 end

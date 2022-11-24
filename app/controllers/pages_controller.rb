fcdd0a7499258426302449f61c75aa0b61ea0dbb
Joshua Smith <jmscodingde@gmail.com>
2022-11-24 19:45:34 +0100

    Navbar, Home page, My Offers Page, Only other peoples offers are viewable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @lawnmowers = Lawnmower.all
+    if params[:query].present?
+      @lawnmowers = Lawnmower.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @lawnmowers = Lawnmower.all
+    end
   end
 end

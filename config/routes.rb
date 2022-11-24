8c14e145145e590ec11f7caa11efbb1aa4e52e4a
Alicia Stromback <aliciastromback@gmail.com>
2022-11-24 17:49:20 +0100

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'lawnmowers#index'
+  root to: 'pages#home'
   resources :lawnmowers do
     resources :bookings, except: [:index]
     resources :reviews
